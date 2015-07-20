223c7de7a85493109c52467b8ef1da0d0ff8c927
InvertGames <invertgamestudios@gmail.com>
2015-07-20 19:50:13 -0400

    Stuff

---
+++ Invert.Attributes/uFrameEvent.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace uFrame.Attributes
+{
+    public class uFrameEvent : Attribute
+    {
+        public string Title { get; set; }
+
+        public uFrameEvent()
+        {
+        }
+
+        public uFrameEvent(string title)
+        {
+            Title = title;
+        }
+
+    }
+
+    public class uFrameCategory : Attribute
+    {
+        public string[] Title { get; set; }
+        public uFrameCategory(params string[] title)
+        {
+            Title = title;
+        }
+
+    }
+}
