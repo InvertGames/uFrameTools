689647f09e35098472ff093db7accdd1ffeb3a5b
nitreo <sinitreo@gmail.com>
2015-08-06 04:35:58 +0200

    add

--- Invert.Core.GraphDesigner.Unity/ElementDesignerStyles.cs
+++ Invert.Core.GraphDesigner.Unity/ElementDesignerStyles.cs
@@ -1765,37 +1765,7 @@ namespace Invert.Common
                 ); }
         }
 
-        public class UnityConnectorStyleSchema : ConnectorStyleSchema
-        {
-            protected override object ConstructTexture(ConnectorSide side, ConnectorDirection direction, bool connected, Color tint = default(Color))
-            {
-
-                string iconBase = null;
-
-                if (direction == ConnectorDirection.Input && connected) iconBase = _filledInputIconCode;
-                if (direction == ConnectorDirection.Input && !connected) iconBase = _emptyInputIconCode;
-                if (direction == ConnectorDirection.Output && connected) iconBase = _filledOutputIconCode;
-                if (direction == ConnectorDirection.Output && !connected) iconBase = _emptyOutputIconCode;
-                if (direction == ConnectorDirection.TwoWay && connected) iconBase = _emptyTwoWayIconCode;
-                if (direction == ConnectorDirection.TwoWay && !connected) iconBase = _filledTwoWayIconCode;
-
-                var baseTexture = GetSkinTexture(iconBase);
-
-                if (tint != default(Color))
-                {
-                    baseTexture = baseTexture.Tint(tint);
-                }
-
-                switch (side)
-                {
-                    default:
-                        return baseTexture;
-                }
-
-                return null;
-            }
-        }
-
+        
        
 
         
